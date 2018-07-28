efa40d3d9b3c453bd0584f5720f9c4637b9f6a05
Unknown <philip.bradstock@simmovation.tech>
2018-07-28 15:57:29 +0100

    Updated init.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from run_generator.tasks import FastSimulationTask, WindGenerationTask
+from run_generator.task_spawner import FastTaskSpawner
+from run_generator.combinators import gridify
+from input_editors.input_editor import InputEditor
+from input_editors.nrel_input_editor import FastInputEditor, TurbSimInputEditor
