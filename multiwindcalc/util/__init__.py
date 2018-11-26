1d93c3eb6453c65d6e743498f06bbc587d34bd95
Mike Tinning <mike@swyftech.com>
2018-11-26 16:51:45 +0000

    Adding test for indexed node

--- multiwindcalc/util/__init__.py
+++ multiwindcalc/util/__init__.py
@@ -1,6 +1,6 @@
 """Utility classes and functions used by ``multiwindcalc``
 """
-from .property import TypedProperty, StringProperty, IntProperty, FloatProperty, typed_property, int_property, string_property, float_property
+from .property import TypedProperty, StringProperty, IntProperty, FloatProperty, ArrayProperty, typed_property, int_property, string_property, float_property
 from .path_builder import PathBuilder
 from .logging import configure_logging
 from .prettyspec import prettyspec
