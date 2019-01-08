1a3b462183d52cd14e7731818b6028a3303dc717
Mike Tinning <mike@swyftech.com>
2019-01-08 16:25:37 +0000

    Fixing license

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # spawn
 # Copyright (C) 2018, Simmovation Ltd.
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
@@ -51,7 +51,7 @@ class ApplyLicenseCommand(Command):
         name = spawn.__name__
         copyright_ = spawn.__copyright__
 
-        license_header = ['# ' + line.strip() + '\n' for line in self.GPL_V3_LICENSE.format(description=name, copyright=copyright_).split('\n')]
+        license_header = [('# ' + line.strip() if line.strip() else '#') + '\n' for line in self.GPL_V3_LICENSE.format(description=name, copyright=copyright_).split('\n')]
 
         files_modified = []
 
